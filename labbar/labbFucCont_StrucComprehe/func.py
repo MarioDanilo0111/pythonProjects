a3762bda2166540bda0ab900956458ea5a9e120e
Mario Fernández <nilovinilo1@gmail.com>
2024-04-07 23:58:35 +0200

    Exercice 2 in Lab 2 Functions

--- labbar/labbFucCont_StrucComprehe/func.py
+++ labbar/labbFucCont_StrucComprehe/func.py
@@ -1,4 +1,4 @@
-def checking_sequenceOfNum(list, sequence):
+""" def checking_sequenceOfNum(list, sequence):
   for i in range(len(list) -len(sequence) + 1):
     if list[i:i+len(sequence)] == sequence:
       return True
@@ -10,4 +10,21 @@ num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 sequence = [1, 2, 3, 4, 5]
 
 is_present = checking_sequenceOfNum(num, sequence)
-print(is_present)
+print(is_present) """
+
+def string_mutatuin(input_string):
+  strin_shup = []
+
+  for i, char in enumerate(input_string):
+    modified_char = char
+    if char.isupper():
+      modified_char = char.lower() 
+
+    if i % 2 == 0:
+        strin_shup.append(modified_char)
+    
+  return ''.join(strin_shup)
+
+string = 'Hello World'
+send_string = string_mutatuin(string)
+print(send_string)
