c1e932a14e089d9aa9f622e8e0ac05f64026447a
Mario Fernández <nilovinilo1@gmail.com>
2024-04-15 09:31:17 +0200

    Virtual environment and Panda install

---
+++ pythonLessons/10Abr/lambda.py
@@ -0,0 +1,24 @@
+# lambda x: x * x 
+
+# def func_lambda(x):
+#   return x * x
+
+# values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# svar = map(lambda x: x + 1, values)
+# print(list(svar))
+
+# print(func_lambda(10))
+
+# def mapper(helper_func, iterable):
+#   result = []
+#   for value in iterable:
+#     helper_result = helper_func(value)
+#     result.append(helper_result)
+
+#   return result
+
+# values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# result = mapper(lambda x: x - 1, values)
+
+# print(list(result))
